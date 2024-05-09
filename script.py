cc74a5c0218b21495f28f28e417689ca442d0e17
mailiisvah <mailiisv@ut.ee>
2024-05-09 15:18:29 +0300

    modified script and added data

--- script.py
+++ script.py
@@ -1,1 +1,15 @@
+import sys
+import argparse
+
 print("hello world")
+
+parser= argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('integers', metavar='N', type=int, nargs='+',
+                    help='an integer for the accumulator')
+
+
+args = parser.parse_args()
+
+print(args.integers)
+print(sum(args.integers)
+      )
