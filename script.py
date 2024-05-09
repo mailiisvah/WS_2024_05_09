0fec92c3d5ec90d606dddc155c5682e791493a12
mailiisvah <mailiisv@ut.ee>
2024-05-09 15:21:15 +0300

    modified script in main branch to create a conflict

--- script.py
+++ script.py
@@ -1,1 +1,10 @@
+import sys
+import os
+
 print("hello world")
+
+print("What is the sum of 2 + 2?")
+#read the answer from the user
+answer = input()
+#print the answer
+print("You answered:" + answer)
